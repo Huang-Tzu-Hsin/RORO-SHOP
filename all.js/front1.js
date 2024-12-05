abe63a48cbc754d8e6f0c04a7d0d13c28b79748f
Huang-Tzu-Hsin <aoa12356@gmail.com>
2024-12-05 10:28:31 +0800

    輪播修正

--- all.js/front1.js
+++ all.js/front1.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = [
-        '../img/盆栽/輪播30.JPG',
+        '../img/盆栽/輪播30.jPG',
         '../img/盆栽/輪播20.jpg',
         '../img/盆栽/輪播10.jpg'
     ];
